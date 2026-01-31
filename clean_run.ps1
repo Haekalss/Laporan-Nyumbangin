7ec7eaa4745afd1f6e3594505f4c5771caa7571f
Haekalsss <kallsss145@gmail.com>
2026-02-01 00:07:52 +0700

    add bab 1 bab 2

--- clean_run.ps1
+++
@@ -1,2 +0,0 @@
-Remove-Item -Path bukped.aux, bukped.log, bukped.toc, bukped.out, bukped.bbl, bukped.blg -ErrorAction SilentlyContinue
-pdflatex -interaction=nonstopmode bukped.tex
--- emergency_fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-taskkill /F /IM pdflatex.exe
-del /Q *.aux *.log *.toc *.out *.bbl *.blg
-pdflatex -interaction=nonstopmode bukped.tex
-bibtex bukped
-pdflatex -interaction=nonstopmode bukped.tex
-pause
